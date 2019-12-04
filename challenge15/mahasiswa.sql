7507a23f4d96aa7c3bc73ed748ef3ea65fbc2cd9
moohat <refixzcorp@gmail.com>
2019-12-04 17:41:16 +0700

    update data mahasiswa.sql add Distinct to row 19

--- challenge15/mahasiswa.sql
+++ challenge15/mahasiswa.sql
@@ -16,11 +16,10 @@ SELECT nama_mhs, sum(sks) as jumlah FROM mahasiswa,kontrak,mata_kuliah WHERE mah
 SELECT nama_mhs,nama_matkul FROM mahasiswa, kontrak, mata_kuliah WHERE mahasiswa.nim = kontrak.nim AND kontrak.id_matkul = mata_kuliah.id_matkul AND nama_matkul = 'Data mining';
 
 /* 6. tampilkan jumlah mahasiswa untuk setiap dosen*/ 
-SELECT nama_dosen, count(nama_mhs) FROM dosen, mahasiswa, kontrak WHERE dosen.id_dosen = kontrak.id_dosen AND mahasiswa.nim = kontrak.nim GROUP BY nama_dosen;
-
+SELECT nama_dosen, COUNT(DISTINCT(nama_mhs)) FROM  mahasiswa left join dosen, kontrak WHERE dosen.id_dosen = kontrak.id_dosen AND mahasiswa.nim = kontrak.nim group by nama_dosen;
 
 /* 7. urutkan mahasiswa berdasarkan umurnya*/ 
-SELECT nama_mhs, umur FROM mahasiswa ORDER BY umur ASC;
+SELECT nama_mhs, umur FROM mahasiswa ORDER BY umur;
 
 /* 8. tampilkan kontrak matakuliah yang harus diulang (nilai D dan E), serta tampilkan data mahasiswa jurusan dan dosen secara lengkap. gunakan mode JOIN dan WHERE clause (solusi terdiri dari 2 syntax SQL)*/ 
 /* WHERE CONDITION */

c5d07bdf9d849d08821babaaac2eebcd4eb0f5a7
moohat <refixzcorp@gmail.com>
2019-12-25 20:23:55 +0700

    modify pagination and filters data

--- ch20/index.js
+++ ch20/index.js
@@ -25,17 +25,7 @@ app.listen(3000, () => {
     console.log("server started (http://localhost:3000/ !");
 });
 
-// GET /
-///url, callback
-// app.get("/", (req, res) => {
-//     const sql = "SELECT * FROM Laporan";
-//     db.all(sql, [], (err, rows) => {
-//         if (err) {
-//             return console.error(err.message);
-//         }
-//         res.render("index", { model: rows });
-//     });
-// });
+
 //add
 app.get('/add', (req, res) =>{
     res.render('add')
@@ -95,25 +85,8 @@ app.post("/edit/:id", (req, res) =>{
     });
 })
 
-//POST /delete/5
+//GET /delete/5
 
-// app.get("/delete/:id", (req, res)=>{
-//     //req.params.* to retrieve the named parameters from the URL (the route)
-//     const id = req.params.id;
-//     const sql = "DELETE FROM Laporan WHERE id = ?";
-//     db.run(sql. id, err =>{
-//         if(err){
-//             console.error(err.message);            
-//         }
-//         console.log(`data ${id} berhasil dihapus`);
-//         res.redirect("/");
-
-        
-//     });
-    
-// });
-
-// delete
 app.get('/delete/:id', (req, res) => {
     let id = req.params.id;
     const sqlDel = `DELETE FROM laporan WHERE id = ?`;
@@ -125,74 +98,148 @@ app.get('/delete/:id', (req, res) => {
 });
 
 
+
+
 app.get('/', (req, res) => {
+    let result = [];
+    let filterData = false;
 
-    //FILTER
-
-    let arrKondisi = [parseInt(req.query.valueID), req.query.valueString, parseInt(req.query.valueInt), req.query.valueFloat, req.query.valueBoolean];
-    let arrIsChecked = [req.query.isID, req.query.isString, req.query.isInt, req.query.isFloat, req.query.isBoolean];
-    let arrField = ["id = ?","instr(data_string,?)","data_integer = ?","data_float = ?","data_boolean = ?","data_date"];
-
-    let activeFilter = [];
-    let activeIndex = [];
-    for(const key in arrKondisi){
-        if (arrIsChecked[key] && arrKondisi[key]){
-            activeFilter.push(arrKondisi[key]);
-            activeIndex.push(Number(key));
-        }
+    if (req.query.check_id && req.query.id) {
+        result.push(`id = ${req.query.id}`);
+        filterData = true;
+    }
+    if (req.query.check_string && req.query.string) {
+        result.push(`data_string = '${req.query.string}'`);
+        filterData = true;
+    }
+    if (req.query.check_integer && req.query.integer) {
+        result.push(`data_integer = ${req.query.integer}`);
+        filterData = true;
+    }
+    if (req.query.check_float && req.query.float) {
+        result.push(`data_float = ${req.query.float}`);
+        filterData = true;
+    }
+    if (req.query.check_date && req.query.startDate && req.query.endDate) {
+        result.push(`data_date BETWEEN '${req.query.startDate}' AND '${req.query.endDate}'`);
+        filterData = true;
+        console.log(req.query)
+    }
+    if (req.query.check_boolean && req.query.boolean) {
+        result.push(`data_boolean = '${req.query.boolean}'`);
+        filterData = true;
+        console.log(req.query);
     }
 
-    if(req.query.isDate && req.query.start){
-        activeFilter.push(`${req.query.start}`);
-        if(req.query.end)
-            activeFilter.push(`${req.query.end}`);
-        activeIndex.push(5);
-    }
+    // count
     
-    //COUNT DATA WITH FILTER
-    let sql = "SELECT count(*) FROM laporan";
-    let filter = false;
-    if(activeFilter.length > 0){
-        sql += " WHERE";
-        filter = true;
-        for(let i = 0; i < activeIndex.length; i++){
-            if (activeIndex[i] != 5){
-                sql += ` ${arrField[activeIndex[i]]}`;
-            } else {
-                if(req.query.end)
-                    sql += ` ${arrField[activeIndex[i]]} BETWEEN ? AND ?`;
-                else
-                    sql += ` ${arrField[activeIndex[i]]} >= ?`;
-            }
-            if(i < activeIndex.length - 1)
-                sql += ` AND`;
-        }
+    let sql = `SELECT COUNT(*) AS total FROM laporan`;
+    if (filterData) {
+        sql += ` WHERE ${result.join(' AND ')}`
     }
+    console.log('sql :'+ sql);
 
-    const currpage = Number(req.query.page) || 1;
-    const limit = 3;
-    const lastquery = req.query;
-    db.all(sql, activeFilter, (err, count) => {
-        const allpage = count[0]['count(*)'];
-        const pages = Math.ceil( allpage / limit);
-        const offset = (currpage - 1) * limit;
-
-        sql = sql.replace("count(*)","*");
+    db.all(sql, (err, count) => {
+        console.log('page ' + req.query.page);
+        
+        const page = req.query.page || 1;
+        // console.log('Page ' + page);
+        const limit = 3;
+        const offset = (page - 1) * limit;
+        // console.log('Offset ' + offset);
+        console.log(req.url);
+        
+        const url = req.url == '/' ? '/?page=1' : req.url
+        // console.log('Url ' + url);
+        const total = count[0].total;
+        // console.log('Total ' + total);
+        const pages = Math.ceil(total / limit);
+        // console.log('Pages ' + pages);
+        let sql = `SELECT * FROM laporan`;
+        if (filterData) {
+            sql += ` WHERE ${result.join(' AND ')}`
+        }
         sql += ` LIMIT ${limit} OFFSET ${offset}`;
 
-        db.all(sql, activeFilter, (err,rows)=>{
-            if(err) return err;
-            res.render('index',{
-                model: rows,
-                query: lastquery,
-                current: currpage,
-                pages: pages,
-                url : req.url
-            })
+        db.all(sql, (err, row) => {
+            res.render('index', {
+                model: row,
+                page,
+                pages,
+                query: req.query,
+                url
+            });
         });
-    
     });
 });
+// app.get('/', (req, res) => {
+
+//     //FILTER
+
+//     let arrKondisi = [parseInt(req.query.valueID), req.query.valueString, parseInt(req.query.valueInt), req.query.valueFloat, req.query.valueBoolean];
+//     let arrIsChecked = [req.query.isID, req.query.isString, req.query.isInt, req.query.isFloat, req.query.isBoolean];
+//     let arrField = ["id = ?","instr(data_string,?)","data_integer = ?","data_float = ?","data_boolean = ?","data_date"];
+
+//     let activeFilter = [];
+//     let activeIndex = [];
+//     for(const key in arrKondisi){
+//         if (arrIsChecked[key] && arrKondisi[key]){
+//             activeFilter.push(arrKondisi[key]);
+//             activeIndex.push(Number(key));
+//         }
+//     }
+
+//     if(req.query.isDate && req.query.start){
+//         activeFilter.push(`${req.query.start}`);
+//         if(req.query.end)
+//             activeFilter.push(`${req.query.end}`);
+//         activeIndex.push(5);
+//     }
+    
+//     //COUNT DATA WITH FILTER
+//     let sql = "SELECT count(*) FROM laporan";
+//     let filter = false;
+//     if(activeFilter.length > 0){
+//         sql += " WHERE";
+//         filter = true;
+//         for(let i = 0; i < activeIndex.length; i++){
+//             if (activeIndex[i] != 5){
+//                 sql += ` ${arrField[activeIndex[i]]}`;
+//             } else {
+//                 if(req.query.end)
+//                     sql += ` ${arrField[activeIndex[i]]} BETWEEN ? AND ?`;
+//                 else
+//                     sql += ` ${arrField[activeIndex[i]]} >= ?`;
+//             }
+//             if(i < activeIndex.length - 1)
+//                 sql += ` AND`;
+//         }
+//     }
+
+//     const currpage = Number(req.query.page) || 1;
+//     const limit = 3;
+//     const lastquery = req.query;
+//     db.all(sql, activeFilter, (err, count) => {
+//         const allpage = count[0]['count(*)'];
+//         const pages = Math.ceil( allpage / limit);
+//         const offset = (currpage - 1) * limit;
+
+//         sql = sql.replace("count(*)","*");
+//         sql += ` LIMIT ${limit} OFFSET ${offset}`;
+
+//         db.all(sql, activeFilter, (err,rows)=>{
+//             if(err) return err;
+//             res.render('index',{
+//                 model: rows,
+//                 query: lastquery,
+//                 current: currpage,
+//                 pages: pages,
+//                 url : req.url
+//             })
+//         });
+    
+//     });
+// });
 
 //TODO: CREATE TABLE AND INSERT INTO TABLE IN JAVASCRIPT
 const sql_create = `CREATE TABLE IF NOT EXISTS laporan (
